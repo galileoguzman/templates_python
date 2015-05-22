837494f9a6571740a8d65f9d1a3efaaf311256d0
Galileo Guzmán <galileoguzman@gmail.com>
2015-05-21 22:37:20 -0500

    console template exec
    Desde un archivo se ejecuta la plantilla y muestra en consola

--- usuarios.py
+++ usuarios.py
@@ -1,3 +1,4 @@
+#utilizando la lectura de un archivo como str le pasamos valores de variables como una plantilla
 with open("templates/usuarios.html", "r") as archivo:
 	plantilla = archivo.read()
 print plantilla.format(nombre="juanito", apellidos="perez")
