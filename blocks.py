cbbc213ecfde0f241895ddca6f18ae495087415c
Galileo Guzmán <galileoguzman@gmail.com>
2015-05-21 23:33:08 -0500

    refactor list in template
    se modifico la forma de asignar variables y concatenar

--- blocks.py
+++ blocks.py
@@ -4,12 +4,10 @@ nombres = {('juanito', 'perez'), ('pedrito', 'martinez'), ('eufemio', 'fernandez
 with open("templates/lista.html", "r") as archivo:
     plantilla = archivo.read()
 html = plantilla.split("<!--block-->")
-block = html[1]
-html[1] = ''
+block = ''
 
 for n in nombres:
-	html[1] += block.format(nombre=n[0],apellido=n[1])
+	block += html[1].format(nombre=n[0],apellido=n[1])
 
 
-print html[1]
-#print html[0] + block + html[2]
+print html[0] + block + html[2]
